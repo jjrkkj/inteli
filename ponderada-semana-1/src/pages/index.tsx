25c0c8c7ac38edc9f084ab46fa528bac2be1a608
Joselito Júnior <joselito.carvalho@sou.inteli.edu.br>
2024-02-09 22:05:14 -0300

    feat: add "add button" and modal

--- ponderada-semana-1/src/pages/index.tsx
+++ ponderada-semana-1/src/pages/index.tsx
@@ -4,6 +4,9 @@ import styles from '@/styles/index.module.css';
 
 export default function Home() {
 	const [data, setData] = useState<any>();
+	const [modalVisibility, setModalVisibility] = useState(false);
+	const [name, setName] = useState<string>();
+	const [message, setMessage] = useState<string>();
 
 	async function getData() {
 		setData((await supabase.from('message').select('*')).data)
@@ -12,6 +15,16 @@ export default function Home() {
 	useEffect(() => {
 		getData();
 	}, [])
+
+	function add() {
+		if (name && message) {
+			setModalVisibility(false);
+			setName(undefined);
+			setMessage(undefined);
+		} else {
+			alert('Preencha os campos obrigatórios, por favor!');
+		}
+	}
 
 	return (
 		<section className={styles['page']}>
@@ -23,6 +36,24 @@ export default function Home() {
 					</div>
 				</>
 			})}
+			<button className={styles['add-button']} onClick={() => setModalVisibility(true)}>
+				+
+			</button>
+
+			{
+				modalVisibility &&
+				<>
+					<div className={styles['bg']} onClick={() => { setModalVisibility(false); setName(undefined); setMessage(undefined) }}></div>
+					<div className={styles['modal']}>
+						<h1>Envie uma mensagem!</h1>
+						<div className={styles['inputs-div']}>
+							<input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Digite seu nome" />
+							<input onChange={(e) => setMessage(e.target.value)} value={message} type="text" placeholder="Digite sua mensagem" />
+						</div>
+						<button onClick={add}>Enviar</button>
+					</div>
+				</>
+			}
 		</section>
 	);
 }
